9626acc476fa7e5b48aafcb917c80d7ab196f69c
Shiv Ratan Saini <shiv.saini@kellton.com>
2026-02-19 16:17:38 +0530

    reday for deployment

--- backend/src/server.js
+++ backend/src/server.js
@@ -2,24 +2,21 @@ import express from "express";
 import dotenv from "dotenv";
 import authRouts from "./routes/auth.route.js";
 import messageRoute from "./routes/message.route.js";
+import path from "path";
 
 dotenv.config();
 const app = express();
 const PORT = process.env.PORT;
-
-// app.get("/api/auth/signup", (req, res) => {
-//   res.send("SignUp endpoint");
-// });
-// app.get("/api/auth/login", (req, res) => {
-//   res.send("Login endpoint");
-// });
-// app.get("/api/auth/logout", (req, res) => {
-//   res.send("Logout endpoint");
-// });
-
+const __dirname = path.resolve();
 app.use("/api/auth", authRouts);
 app.use("/api/messages", messageRoute);
 
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend/dist/index.html"));
+  });
+}
 app.listen(PORT, () => {
   console.log(`server is running at port ${PORT} `);
 });
